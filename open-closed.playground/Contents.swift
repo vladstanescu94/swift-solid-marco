00549d6cc67c3a8b85981183b50dd2d1774832b0
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-24 09:55:37 +0200

    Add Logger class to print some data

--- open-closed.playground/Contents.swift
+++ open-closed.playground/Contents.swift
@@ -1,1 +1,30 @@
 import Foundation
+
+class Logger {
+    
+    func printData() {
+        let cars = [
+            Car(name: "Batmobile", color: "Black"),
+            Car(name: "SuperCar", color: "Gold"),
+            Car(name: "FamilyCar", color: "Grey")
+        ]
+        
+        cars.forEach { car in
+            print(car.printDetails())
+        }
+    }
+}
+
+class Car {
+    let name: String
+    let color: String
+    
+    init(name: String, color: String) {
+        self.name = name
+        self.color = color
+    }
+    
+    func printDetails() -> String {
+        return "I'm \(name) and my color is \(color)"
+    }
+}
