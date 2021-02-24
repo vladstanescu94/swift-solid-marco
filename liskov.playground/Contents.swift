541e7d968ca1b48d58b7b0e15d0ce4f448ca069f
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-24 11:01:32 +0200

    Add Rectangle and Square class that breaks LSP

--- liskov.playground/Contents.swift
+++ liskov.playground/Contents.swift
@@ -8,3 +8,34 @@ class Handler {
         // Save string in the Cloud
     }
 }
+
+class Rectangle {
+
+    var width: Float = 0
+    var length: Float = 0
+
+    var area: Float {
+        return width * length
+    }
+}
+
+class Square: Rectangle {
+
+    override var width: Float {
+        didSet {
+            length = width
+        }
+    }
+}
+
+func printArea(of rectangle: Rectangle) {
+    rectangle.length = 5
+    rectangle.width = 2
+    print(rectangle.area)
+}
+
+let rectangle = Rectangle()
+printArea(of: rectangle) // 10
+
+let square = Square()
+printArea(of: square) // 4
