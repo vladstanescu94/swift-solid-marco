70ce53af7fdcfa9466b0d46ee8101e694c8b71a9
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-24 09:57:18 +0200

    Add Bicycle class with another implementation of printDetails()

--- open-closed.playground/Contents.swift
+++ open-closed.playground/Contents.swift
@@ -1,16 +1,25 @@
 import Foundation
 
 class Logger {
-    
+
     func printData() {
         let cars = [
             Car(name: "Batmobile", color: "Black"),
             Car(name: "SuperCar", color: "Gold"),
             Car(name: "FamilyCar", color: "Grey")
         ]
-        
+
         cars.forEach { car in
             print(car.printDetails())
+        }
+
+        let bicycles = [
+            Bicycle(type: "BMX"),
+            Bicycle(type: "Tandem")
+        ]
+
+        bicycles.forEach { bicycles in
+            print(bicycles.printDetails())
         }
     }
 }
@@ -18,13 +27,25 @@ class Logger {
 class Car {
     let name: String
     let color: String
-    
+
     init(name: String, color: String) {
         self.name = name
         self.color = color
     }
-    
+
     func printDetails() -> String {
         return "I'm \(name) and my color is \(color)"
+    }
+}
+
+class Bicycle {
+    let type: String
+
+    init(type: String) {
+        self.type = type
+    }
+
+    func printDetails() -> String {
+        return "I'm a \(type)"
     }
 }
