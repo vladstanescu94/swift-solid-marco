804a5cd836be874b4a05e7498455c3b9725b6b68
Vlad Stanescu <vladstanescu94@gmail.com>
2021-02-24 10:52:41 +0200

    Add FilteredHandler that breaks LSP

--- liskov.playground/Contents.swift
+++ liskov.playground/Contents.swift
@@ -1,1 +1,17 @@
 import Foundation
+
+class Handler {
+
+    func save(string: String) {
+        // Save string in the Cloud
+    }
+}
+
+class FilteredHandler: Handler {
+
+    override func save(string: String) {
+        guard string.count > 5 else { return }
+
+        super.save(string: string)
+    }
+}
