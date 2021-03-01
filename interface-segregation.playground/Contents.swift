6a99d11e375493117c21b22a3a28db6468eda383
Vlad Stanescu <vladstanescu94@gmail.com>
2021-03-01 09:11:59 +0200

    Add fat class

--- interface-segregation.playground/Contents.swift
+++ interface-segregation.playground/Contents.swift
@@ -31,3 +31,22 @@ class PoorButton: TapProtocol {
         // send tap
     }
 }
+
+// MARK: - Fat Class
+
+class Video {
+    var title: String = "My Video"
+    var description: String = "This is a beautiful video"
+    var author: String = "Vlad Stanescu"
+    var url: String = "https://vladstanescu.ro/my_video"
+    var duration: Int = 60
+    var created: Date = Date()
+    var update: Date = Date()
+}
+
+func play(video: Video) {
+    // load the player UI
+    // load the content at video.url
+    // add video.title to the player UI title
+    // update the player scrubber with video.duration
+}
