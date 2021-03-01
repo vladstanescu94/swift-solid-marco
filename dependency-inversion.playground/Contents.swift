67aa846b7d38b73750d958095c8d70b2e47bda9b
Vlad Stanescu <vladstanescu94@gmail.com>
2021-03-01 09:31:11 +0200

    Add Storage Protocol to fix dependency

--- dependency-inversion.playground/Contents.swift
+++ dependency-inversion.playground/Contents.swift
@@ -1,19 +1,36 @@
 import Foundation
 
+protocol Storage {
+    func save(string: String)
+}
+
 class Handler {
     
-    let fm = FilesystemManager()
+    let storage: Storage
+    
+    init(storage: Storage) {
+        self.storage = storage
+    }
     
     func handle(string: String) {
-        fm.save(string: string)
+        storage.save(string: string)
     }
 }
 
-class FilesystemManager {
-    
+class FilesystemManager: Storage {
+
     func save(string: String) {
-        // Open a file
+        // Open a file in read-mode
         // Save the string in this file
         // Close the file
+    }
+}
+
+class DatabaseManager: Storage {
+
+    func save(string: String) {
+        // Connect to the database
+        // Execute the query to save the string in a table
+        // Close the connection
     }
 }
